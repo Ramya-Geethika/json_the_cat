95a1c922f4faf5f11c129d0193b1b231d2165b31
Ramya-Geethika <geethikakramya@gmail.com>
2020-08-26 23:04:45 +0000

    Cat as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,13 +2,11 @@ const request = require('request');
 const breed = process.argv.slice(2);
 let url = "https://api.thecatapi.com/v1/breeds/search?q=";
 let finalurl = url + breed;
-let data;
-
 request (finalurl, (error, response, body) => {
   if(error){
     console.log("error on loading the page");
   }
-  data = JSON.parse(body);
+  let data = JSON.parse(body);
   if(data == ""){
     console.log("could not find the breed");
   }
